5494c787ca93a015aecbe6e5a17d27788f3e6a83
y0da822 <anthonyjpetrone@gmail.com>
2020-05-24 07:49:19 -0400

    v1.3 added a local log file because program still isnt auto switching to new log file when IIS creates a new one

--- IISDiscordBot.py
+++ IISDiscordBot.py
@@ -9,6 +9,19 @@ import pytz
 from tzlocal import get_localzone
 from discord import Webhook, RequestsWebhookAdapter
 
+
+# local log file function
+def write_log(log_msg, file_name):
+    print("In write_log function - " + file_name)
+
+    with open(file_name, 'a') as log:
+        log.write("{0},{1}\n".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), str(log_msg)))
+
+
+# create local log file
+my_log_file = "LOG-" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".csv"
+print("My log file is " + my_log_file)
+
 # config parser - to read from config file
 parser = configparser.ConfigParser()
 parser.read('iisdiscordbot.config')
@@ -18,13 +31,16 @@ log_file_path = parser.get('iis', 'logfilepath')
 log_file_path_files = glob.glob(log_file_path + "/*.log")
 log_file_latest = max(log_file_path_files, key=os.path.getctime)
 log_file = open(log_file_latest, "r")
+write_log("Opened IIS log file " + log_file.name)
 
 # get local timezone for utc conversion
 local_tz = get_localzone()
+write_log("Getting local timezone for IIS log date/time conversion")
 
 # connect to discord webhook
 webhook = Webhook.partial(parser.get('discord', 'webhookid'), parser.get('discord', 'webhooktoken'), \
                           adapter=RequestsWebhookAdapter())
+write_log("Connected to Discord webhook ID (" + parser.get('discord', 'webhookid') + ")")
 
 while 1:
     # check for the latest log file each iteration
@@ -32,7 +48,7 @@ while 1:
     if log_file.name != log_file_latest:
         log_file.close()
         log_file = open(log_file_latest, "r")
-        print("Log file has been rotated to new one " + log_file.name)
+        write_log("Log file has been rotated to new one " + log_file.name)
     where = log_file.tell()
     log_line = log_file.readline()
     if not log_line:
@@ -48,9 +64,11 @@ while 1:
             datetime_obj = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
             dt_downloaded = datetime_obj.replace(tzinfo=pytz.utc).astimezone(local_tz)
             file_path = log_line_list[len(log_line_list) - 1].replace("+", " ")
-            print(
-                log_file_latest + ": " + user + " has downloaded " + file_downloaded + " on " + dt_downloaded.strftime("%Y-%m-%d %H:%M:%S %Z%z") + " from " + file_path)
-            webhook.send(user + " has downloaded " + file_path + " on " + dt_downloaded.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+            write_log(
+                log_file_latest + ": " + user + " has downloaded " + file_downloaded + " on " + dt_downloaded.strftime(
+                    "%Y-%m-%d %H:%M:%S %Z%z") + " from " + file_path)
+            webhook.send(
+                user + " has downloaded " + file_path + " on " + dt_downloaded.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
         elif "STOR" in log_line:
             log_line_list = log_line.split()
             user = log_line_list[3]
@@ -59,6 +77,7 @@ while 1:
             datetime_obj = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
             dt_uploaded = datetime_obj.replace(tzinfo=pytz.utc).astimezone(local_tz)
             file_path = log_line_list[len(log_line_list) - 1].replace("+", " ")
-            print(
-                log_file_latest + ": " + user + " has uploaded " + file_uploaded + " on " + dt_uploaded.strftime("%Y-%m-%d %H:%M:%S %Z%z") + " from " + file_path)
+            write_log(
+                log_file_latest + ": " + user + " has uploaded " + file_uploaded + " on " + dt_uploaded.strftime(
+                    "%Y-%m-%d %H:%M:%S %Z%z") + " from " + file_path)
             webhook.send(user + " has uploaded " + file_path + " on " + dt_uploaded.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
